f926d3a1b24adf5de5a032f1546a8841fbfda5ec
Eugene <fedoriuk2001@gmail.com>
2022-07-17 09:16:52 +0200

    first commit

---
+++ _next/static/gvzYSklS4bbRKMPXqbqsT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fcontact","\u002F","\u002Fblog","\u002Fnewsletter","\u002Fprojects","\u002Fservices","\u002Fprojects\u002F[slug]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
